972a1f116259530d668f3bf0a44ede66e7832ddd
Emily Sellers <emilymelindasellers@gmail.com>
2022-11-26 15:02:51 -0800

    POST { email, password } to /api/v1/users/sessions signs in an existing user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,17 +4,6 @@ const request = require('supertest');
 const app = require('../lib/app');
 // const UserService = require('../lib/services/UserService');
 
-// const registerAndLogin = async (userProps = {}) => {
-//   const password = userProps.password ?? mockUser.password;
-//   const agent = request.agent(app);
-//   const user = await UserService.create({ ...mockUser, ...userProps });
-//   const { email } = user;
-//   await (
-//     await agent.post('./api/v1/users/sessions')
-//   ).setEncoding({ email, password });
-//   return [agent, user];
-// };
-
 describe('users routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -27,7 +16,7 @@ describe('users routes', () => {
     password: '123456',
   };
 
-  it('POST /api/vi/users creates a new user', async () => {
+  it('POST /api/v1/users creates a new user', async () => {
     const res = await request(app).post('/api/v1/users').send(mockUser);
     expect(res.status).toBe(200);
     const { firstName, lastName, email } = mockUser;
@@ -38,6 +27,14 @@ describe('users routes', () => {
       lastName,
       email,
     });
+  });
+
+  it('POST /api/v1/sessions signs in an existing user', async () => {
+    await request(app).post('/api/v1/users').send(mockUser);
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ email: 'test@test.com', password: '123456' });
+    expect(res.status).toEqual(200);
   });
 
   afterAll(() => {
